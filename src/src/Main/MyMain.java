cac3e143c2696cf367f0aba42fec0d4a29ae8de3
Андрей <akovzika@yandex.by>
2025-10-11 21:07:06 +0300

    Added trird and fourth homework with arrays, Scanner and loops

--- src/src/Main/MyMain.java
+++ src/src/Main/MyMain.java
@@ -1,5 +1,8 @@
 package Main;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class MyMain {
     public static void main(String[] args) {
         printNameAge();
@@ -15,6 +18,23 @@ public class MyMain {
         dividingWithRemainder(21, 8);
         System.out.println("-".repeat(50));
         switchValues(3, 5);
+        System.out.println();
+        System.out.println("-".repeat(50));
+        System.out.println("HOMEWORK 3");
+        System.out.println("-".repeat(50));
+        checkEven(6);
+        System.out.println("-".repeat(50));
+        checkFeelTemp(20);
+        System.out.println("-".repeat(50));
+        printSquares();
+        System.out.println("-".repeat(50));
+        printArithmeticProgression(7);
+        System.out.println("-".repeat(50));
+        sumToEnteredNum();
+        System.out.println("-".repeat(50));
+        System.out.println("HOMEWORK 4");
+        System.out.println("-".repeat(50));
+        changeTheArray();
     }
 
 
@@ -86,7 +106,7 @@ public class MyMain {
     }
 
     //Задача *:
-    //Написать программу которая будет менять местами значение целочисленных
+    //Написать программу, которая будет менять местами значение целочисленных
     //переменных.
     public static void switchValues(int a, int b) {
         int c =a;
@@ -94,5 +114,187 @@ public class MyMain {
         b = c;
         System.out.println(a);
         System.out.println(b);
+    }
+
+    //Задача 1:
+    //Напишите программу, которая будет принимать на вход число из консоли и на выход
+    //будет выводить сообщение четное число или нет. Для определения четности числа
+    //используйте операцию получения остатка от деления (операция выглядит так: '% 2').
+    public static void checkEven(int n) {
+        System.out.println((n % 2 == 0) ? "Even" : "Odd");
+    }
+
+    //Задача 2:
+    //Для введенного числа t (температура на улице) вывести: Если t>–5, то вывести «Warm».
+    //Если –5>= t > –20, то вывести «Normal». Если –20>= t, то вывести «Cold».
+    public static void checkFeelTemp(int t) {
+        if ( t > -5) {
+            System.out.println("Warm");
+        } else if (t <= -5 && t > -20) {
+            System.out.println("Normal");
+        } else {
+            System.out.println("Cold");
+        }
+    }
+
+    //Задача 3:
+    //Составьте программу, выводящую на экран квадраты чисел от 10 до 20 включительно.
+    public static void printSquares() {
+        for (int i = 10; i <= 20; i++) {
+            System.out.print(i * i + " ");
+        }
+        System.out.println();
+    }
+
+    //Задача 4:
+    //Необходимо, чтоб программа выводила на экран вот такую последовательность:
+    //7 14 21 28 35 42 49 56 63 70 77 84 91 98. В решении используйте цикл while.
+    public static void printArithmeticProgression(int n) {
+        int sum = n;
+        while (sum < 100) {
+            System.out.print(sum + " ");
+            sum += n;
+        }
+        System.out.println();
+    }
+
+    //Задача *:
+    //Напишите программу, где пользователь вводит любое целое положительное число. А
+    //программа суммирует все числа от 1 до введенного пользователем числа. Для ввода
+    //числа воспользуйтесь классом Scanner. Сделать проверку, чтобы пользователь не мог
+    //ввести некорректные данные.
+    public static void sumToEnteredNum() {
+        System.out.println("Enter the number please");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n < 0) {
+            System.out.println("INCORRECT DATA!");
+        } else {
+            int sum = 0;
+            for (int i = 0; i <= n; i++) {
+                sum += i;
+            }
+            System.out.println("The sum of numbers from 1 to" + n + " equals: " + sum);
+        }
+    }
+
+    //Задача 1:
+    //Пройти по массиву, вывести все элементы в прямом и в обратном порядке.
+    public static void changeTheArray() {
+        Scanner sc = new Scanner(System.in);
+        Random rnd = new Random();
+        System.out.println("Enter the length of array please");
+        int n = sc.nextInt();
+        int[] intArray = new int[n];
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = rnd.nextInt(10);
+        }
+
+        //Задача 1:
+        //Пройти по массиву, вывести все элементы в прямом и в обратном порядке.
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println("Element " + i + ": "+ intArray[i]);
+        }
+        System.out.println();
+        for (int i = intArray.length - 1; i >= 0; i--) {
+            System.out.println("Element " + i + ": "+ intArray[i]);
+        }
+        System.out.println();
+
+        //Задача 2:
+        //Найти минимальный-максимальный элементы и вывести в консоль.
+        int min = intArray[0];
+        int max = intArray[0];
+        int minIndex = 0;
+        int maxIndex = 0;
+        /*for (int i : intArray) {
+            if (i < min) {
+                min = i;
+            } else if (i > max) {
+                max = i;
+            }
+        }*/
+        for (int i = 0; i < intArray.length; i++) {
+            if (intArray[i] < min) {
+                min = intArray[i];
+                minIndex = i;
+            } else if (intArray[i] > max) {
+                max = intArray[i];
+                maxIndex = i;
+            }
+        }
+        System.out.println("Minimum value is " + min + "\nMaximum value is " + max);
+        System.out.println();
+
+        //Задача 3:
+        //Найти индексы минимального и максимального элементов и вывести в консоль.
+        System.out.println("Index of minimum value is " + minIndex + "\nIndex of maximum value is " + maxIndex);
+        System.out.println();
+
+        //Задача 4:
+        //Найти и вывести количество нулевых элементов. Если нулевых элементов нет - вывести
+        //сообщение, что их нет.
+        int sumOfZeroElements = 0;
+        for (int i : intArray) {
+            if (i == 0) sumOfZeroElements++;
+        }
+        System.out.println(sumOfZeroElements == 0 ? "There is no zero elements!" : "Number of zero elements is: "
+                + sumOfZeroElements);
+        System.out.println();
+
+        //Задача 5:
+        //Пройти по массиву и поменять местами элементы первый и последний, второй и
+        //предпоследний и т.д.
+        int x;
+        for (int i = 0, k = intArray.length - 1; i < k; i++, k--) {
+            x = intArray[i];
+            intArray[i] = intArray[k];
+            intArray[k] = x;
+        }
+        for (int i : intArray) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        //Задача 6:
+        //Проверить, является ли массив возрастающей последовательностью (каждое следующее
+        //число больше предыдущего).
+        boolean isIncreasing = false;
+        for (int i = 0; i < intArray.length - 1; i++) {
+            if (intArray[i] > intArray[i + 1]) {
+                break;
+            }
+            if (i == intArray.length - 1){
+                isIncreasing = true;
+            }
+        }
+        System.out.println(isIncreasing ? "This array is an increasing sequence!" :
+                "This array is not an increasing sequence!");
+        System.out.println();
+
+        //Задача *:
+        //Имеется массив из неотрицательных чисел(любой). Представьте что массив
+        //представляет целое число (Например массив {1,2,3} -> 123, {9,9,9} -> 999). Задача
+        //добавить единицу к этому “числу” и на выходе получить исправленный массив. Массив не
+        //содержит нуля в начале, кроме самого числа 0.
+        System.out.println("How many digits in your number?");
+        int[] arrayWithStar = new int[sc.nextInt()];
+        System.out.println("Enter the number digit by digit");
+        for (int i = 0; i < arrayWithStar.length; i++) {
+            arrayWithStar[i] = sc.nextInt();
+        }
+        int nmb = 0;
+        for (int i = 0; i < arrayWithStar.length; i++) {
+            nmb = nmb * 10 + arrayWithStar[i];
+        }
+        nmb++;
+        String str = "" + nmb;
+        int[] newArrWithStar = new int[str.length()];
+        for (int i = 0; i < newArrWithStar.length; i++) {
+            newArrWithStar[i] = Character.getNumericValue(str.charAt(i));
+        }
+        for (int i : newArrWithStar) {
+            System.out.print(i + " ");
+        }
     }
 }
