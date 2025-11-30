f71da822ee322be0640a61c09d58405f19aa5930
Андрей <akovzika@yandex.by>
2025-11-30 20:27:50 +0300

    fixied task with serialization

--- src/src/Serialize/Main.java
+++ src/src/Serialize/Main.java
@@ -6,13 +6,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(new File(
-                "D:\\old hdd\\DEV\\javaIntellij\\MyMainTMS\\src\\src\\Serialize\\arguments.txt"));
-        ArrayList<String> arguments = new ArrayList<String>();
-        while (sc.hasNextLine()) {
-            arguments.add(sc.next());
-        }
-        Terminal wideParams = new Terminal(arguments.toArray(new String[0]));
+//        Scanner sc = new Scanner(new File(
+//                "D:\\old hdd\\DEV\\javaIntellij\\MyMainTMS\\src\\src\\Serialize\\arguments.txt"));
+//        ArrayList<String> arguments = new ArrayList<String>();
+//        while (sc.hasNextLine()) {
+//            arguments.add(sc.next());
+//        }
+//        Terminal wideParams = new Terminal(arguments.toArray(new String[0]));
+        Terminal wideParams = new Terminal(args);
 
         FileOutputStream fileOutputStream = new FileOutputStream(
                 "D:\\old hdd\\DEV\\javaIntellij\\MyMainTMS\\src\\src\\Serialize\\serialized_object");
